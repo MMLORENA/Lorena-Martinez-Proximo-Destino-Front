f0e445cbc8123e8a7f6f5b472e3d7324862cac8e
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-08-31 14:09:06 +0200

    tested RegisterPage

--- src/pages/RegisterPage/RegisterPage.test.tsx
+++ src/pages/RegisterPage/RegisterPage.test.tsx
@@ -9,5 +9,21 @@ describe("Given a RegistertPage component", () => {
 
       expect(resultTitle).toBeInTheDocument();
     });
+
+    test("It should have a form  inside with 6 inputs", () => {
+      render(<RegisterPage />);
+      const form = screen.getByText("Usuario");
+
+      expect(form).toBeInTheDocument();
+    });
+
+    test("It should have a image with alt text  with 'estrellas Via Lactea'", () => {
+      const expectedAltText = "estrellas Via Lactea";
+      render(<RegisterPage />);
+      const resultImage: HTMLImageElement =
+        screen.getByAltText(expectedAltText);
+
+      expect(resultImage).toHaveProperty("alt");
+    });
   });
 });

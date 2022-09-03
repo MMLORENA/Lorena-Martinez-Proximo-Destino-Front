c23c2c45af80c8fe98702b2b50a52107f144cbf3
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-09-03 22:28:16 +0200

    tested getLogin

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,24 @@
 import { rest } from "msw";
 
+const apiUrl = process.env.REACT_APP_API_URL as string;
 export const handlers = [
-  rest.post(
-    `${process.env.REACT_APP_API_URL}user/register`,
-    async (req, res, ctx) => {
-      const { userName } = await req.json();
-      const status = userName === "" ? 400 : 201;
+  rest.post(`${apiUrl}user/register`, async (req, res, ctx) => {
+    const { userName } = await req.json();
+    const status = userName === "" ? 400 : 201;
 
-      return res(ctx.status(status));
-    }
-  ),
+    return res(ctx.status(status));
+  }),
+
+  rest.post(`${apiUrl}user/login`, async (req, res, ctx) => {
+    const { userName } = await req.json();
+
+    const status = userName === "" ? 403 : 200;
+
+    return res(
+      ctx.status(status),
+      ctx.json({
+        user: { token: "#" },
+      })
+    );
+  }),
 ];

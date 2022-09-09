84f51811bc7b1233c23740929d7d51125a4f23b5
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-09-09 15:29:32 +0200

    created deleteDestinations & tested

--- src/store/reducer/destinationsSlice/destinationsSlice.test.ts
+++ src/store/reducer/destinationsSlice/destinationsSlice.test.ts
@@ -1,5 +1,7 @@
+import destinationsTest from "../../../test-utils/mocksDestinations/mockDestinations";
 import { Destinations } from "../../models/Destinations";
 import {
+  deleteDestinationActionCreator,
   destinationsReducer,
   loadDestinationsActionCreator,
   unloadDestinationsActionCreator,
@@ -8,22 +10,10 @@ import {
 describe("Given a destinationsSlice", () => {
   const previousDestinationsState: Destinations = [];
 
-  describe("And an loadDestinationActionCreator", () => {
+  describe("And an loadDestinationsActionCreator", () => {
     describe("When it's receives a previous state", () => {
       test("Then should return a destinations list with the destination given", () => {
-        const expectedDestinations: Destinations = [
-          {
-            destination: "Nepal",
-            image: "./",
-            latitude: 200,
-            longitud: 24,
-            cateogry: "Adventure",
-            firstPlan: "Himalaya",
-            descriptionFirstPlan: "Trekking",
-            id: "0",
-          },
-        ];
-
+        const expectedDestinations: Destinations = destinationsTest;
         const destinationsSliceState = destinationsReducer(
           previousDestinationsState,
           loadDestinationsActionCreator(expectedDestinations)
@@ -34,7 +24,7 @@ describe("Given a destinationsSlice", () => {
     });
   });
 
-  describe("And an unloadDestinationActionCreator", () => {
+  describe("And an unloadDestinationsActionCreator", () => {
     describe("When it's called", () => {
       test("Then should return a destinations list empty", () => {
         const expectedDestinations: Destinations = [];
@@ -42,6 +32,22 @@ describe("Given a destinationsSlice", () => {
         const destinationsSliceState = destinationsReducer(
           previousDestinationsState,
           unloadDestinationsActionCreator()
+        );
+
+        expect(destinationsSliceState).toStrictEqual(expectedDestinations);
+      });
+    });
+  });
+
+  describe("And an deletedDestinationActionCreator", () => {
+    describe("When it's called with a destination id and a destinations list", () => {
+      test("Then should return a destinations list without the id destination received", () => {
+        const idDeletingDestination = "63175bcd3349cd8da4ca9dbd";
+        const expectedDestinations: Destinations = [];
+
+        const destinationsSliceState = destinationsReducer(
+          previousDestinationsState,
+          deleteDestinationActionCreator(idDeletingDestination)
         );
 
         expect(destinationsSliceState).toStrictEqual(expectedDestinations);

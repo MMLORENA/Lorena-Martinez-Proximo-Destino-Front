7b6acc35098152ea24bd564e0ee3a337312e2a13
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-09-12 18:29:38 +0200

    added button to create page

--- src/pages/DestinationsPage/DestinationsPage.tsx
+++ src/pages/DestinationsPage/DestinationsPage.tsx
@@ -1,11 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
 import DestinationsList from "../../components/DestinationsList/DestinationsList";
 import Logo from "../../components/Logo/Logo";
 import DestinationPageStyled from "./DestinationsPageStyled";
 
 const DestinationsPage = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate("/crear");
+  };
+
   return (
     <DestinationPageStyled>
       <Logo />
+      <Button
+        classNameTypeButton="small"
+        buttonText="Nuevo Destino"
+        actionOnclick={handleOnClick}
+        type="button"
+        isDisabled={false}
+      />
       <DestinationsList />
     </DestinationPageStyled>
   );
